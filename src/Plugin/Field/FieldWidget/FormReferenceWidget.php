2caf272241df00cfe221a7411ab5510a9c319cda
Eric Aguayo <ericmaster@nimblersoft.com>
2025-06-04 16:14:06 +0000

    Allow referencing entities as arguments

--- src/Plugin/Field/FieldWidget/FormReferenceWidget.php
+++ src/Plugin/Field/FieldWidget/FormReferenceWidget.php
@@ -19,7 +19,8 @@ use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
  *   }
  * )
  */
-class FormReferenceWidget extends WidgetBase implements ContainerFactoryPluginInterface {
+class FormReferenceWidget extends WidgetBase implements ContainerFactoryPluginInterface
+{
 
   /**
    * @var \Drupal\form_reference_field\FormReferenceFormDiscovery
@@ -29,7 +30,8 @@ class FormReferenceWidget extends WidgetBase implements ContainerFactoryPluginIn
   /**
    * Constructs a CustomWidget object.
    */
-  public function __construct($plugin_id, $plugin_definition, $field_definition, $settings, $third_party_settings, $formDiscovery) {
+  public function __construct($plugin_id, $plugin_definition, $field_definition, $settings, $third_party_settings, $formDiscovery)
+  {
     parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $third_party_settings);
     $this->formDiscovery = $formDiscovery;
   }
@@ -37,7 +39,8 @@ class FormReferenceWidget extends WidgetBase implements ContainerFactoryPluginIn
   /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition)
+  {
     return new static(
       $plugin_id,
       $plugin_definition,
@@ -64,25 +67,73 @@ class FormReferenceWidget extends WidgetBase implements ContainerFactoryPluginIn
       '#required' => $element['#required'],
     ];
     // Add a multiple value field for arguments with add more functionality.
-    $form_args = isset($items[$delta]->form_args) && is_array($items[$delta]->form_args) ? $items[$delta]->form_args : [''];
+    // Get form_args from form_state if available, otherwise from $items.
+    $form_args = NULL;
+    // Try to get from form_state (handles both AJAX and normal submissions).
+    $parents = $element['#field_parents'] ?? [];
+    $parents = array_merge($parents, [$items->getName(), $delta, 'form_args']);
+    $form_args = $form_state->getValue($parents);
+    if ($form_args === NULL) {
+      $form_args = isset($items[$delta]->form_args) && is_array($items[$delta]->form_args) ? $items[$delta]->form_args : [''];
+    }
     $element['form_args'] = [
       '#type' => 'fieldset',
       '#title' => $this->t('Form arguments (tokens allowed)'),
       '#description' => $this->t('Enter one or more arguments to pass to the form. Use tokens if needed.'),
       '#tree' => TRUE,
     ];
+    // Add token browser if token module is available.
+    if (\Drupal::moduleHandler()->moduleExists('token')) {
+      $element['form_args']['token_browser'] = [
+        '#theme' => 'token_tree_link',
+        '#token_types' => ['node', 'user', 'file', 'comment', 'taxonomy_term', 'block'],
+        '#global_types' => TRUE,
+        '#dialog' => TRUE,
+        '#weight' => 100,
+      ];
+    }
     $max_args = $form_state->get(['form_reference_field', 'max_args', $delta]) ?? count($form_args);
     if ($form_state->getTriggeringElement() && $form_state->getTriggeringElement()['#name'] === 'add_form_arg_' . $delta) {
       $max_args++;
       $form_state->set(['form_reference_field', 'max_args', $delta], $max_args);
     }
     for ($i = 0; $i < $max_args; $i++) {
+      $arg_type = isset($form_args[$i]['type']) ? $form_args[$i]['type'] : 'text';
       $element['form_args'][$i] = [
-        '#type' => 'textfield',
-        '#title' => $this->t('Argument @num', ['@num' => $i + 1]),
-        '#default_value' => isset($form_args[$i]) ? $form_args[$i] : '',
-        '#required' => FALSE,
+        '#type' => 'container',
+        '#attributes' => ['class' => ['form-argument-wrapper']],
       ];
+      $element['form_args'][$i]['type'] = [
+        '#type' => 'select',
+        '#title' => $this->t('Argument @num type', ['@num' => $i + 1]),
+        '#options' => [
+          'text' => $this->t('Text/Token'),
+          'entity' => $this->t('Entity Reference'),
+        ],
+        '#default_value' => $arg_type,
+        '#ajax' => [
+          'callback' => [get_class($this), 'argumentTypeAjax'],
+          'wrapper' => 'form-arg-type-wrapper-' . $delta . '-' . $i,
+        ],
+      ];
+      $element['form_args'][$i]['#prefix'] = '<div id="form-arg-type-wrapper-' . $delta . '-' . $i . '">';
+      $element['form_args'][$i]['#suffix'] = '</div>';
+      if ($arg_type === 'entity') {
+        $element['form_args'][$i]['entity'] = [
+          '#type' => 'entity_autocomplete',
+          '#target_type' => 'node', // TODO: make configurable.
+          '#title' => $this->t('Entity for argument @num', ['@num' => $i + 1]),
+          '#default_value' => isset($form_args[$i]['entity']) ? $form_args[$i]['entity'] : NULL,
+          '#required' => FALSE,
+        ];
+      } else {
+        $element['form_args'][$i]['text'] = [
+          '#type' => 'textfield',
+          '#title' => $this->t('Argument @num', ['@num' => $i + 1]),
+          '#default_value' => isset($form_args[$i]['text']) ? $form_args[$i]['text'] : (is_string($form_args[$i]) ? $form_args[$i] : ''),
+          '#required' => FALSE,
+        ];
+      }
     }
     $element['form_args']['add_more'] = [
       '#type' => 'submit',
@@ -104,7 +155,8 @@ class FormReferenceWidget extends WidgetBase implements ContainerFactoryPluginIn
   /**
    * Ajax callback for add more arguments.
    */
-  public static function addMoreAjax(array $form, FormStateInterface $form_state) {
+  public static function addMoreAjax(array $form, FormStateInterface $form_state)
+  {
     // Find the triggering element's parents to return the correct part of the form.
     $triggering_element = $form_state->getTriggeringElement();
     $parents = $triggering_element['#array_parents'];
@@ -116,7 +168,8 @@ class FormReferenceWidget extends WidgetBase implements ContainerFactoryPluginIn
   /**
    * Submit handler for add more arguments.
    */
-  public static function addMoreSubmit(array $form, FormStateInterface $form_state) {
+  public static function addMoreSubmit(array $form, FormStateInterface $form_state)
+  {
     // No-op, state is handled in formElement.
   }
 
@@ -182,5 +235,17 @@ class FormReferenceWidget extends WidgetBase implements ContainerFactoryPluginIn
       );
     }
     return $summary;
+  }
+
+  /**
+   * Ajax callback for switching argument type.
+   */
+  public static function argumentTypeAjax(array $form, FormStateInterface $form_state)
+  {
+    $triggering_element = $form_state->getTriggeringElement();
+    $parents = $triggering_element['#array_parents'];
+    array_pop($parents); // Remove the select itself.
+    $element = \Drupal\Component\Utility\NestedArray::getValue($form, $parents);
+    return $element;
   }
 }
